7a0456a3c6660b0ad34c0e8913303769bebae75f
AndrMshk <andrmshk1@gmail.com>
2023-03-26 16:38:49 +0300

    add token to file

--- weather.js
+++ weather.js
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
 
 import { getArgs } from './helpers/args.js'
-import { printHelp } from './services/log_service.js'
+import { printHelp, printSucsess, printError } from './services/log_service.js'
 import { seveKeyValue } from './services/storage.servise.js'
+
+const saveToken = async (token) => {
+    try {
+        await seveKeyValue('token', token)
+        printSucsess('Token saved')
+    } catch (err) {
+        printError(err.message)
+    }
+}
 
 const initCLI = () => {
     const args = getArgs(process.argv)
@@ -13,7 +22,7 @@ const initCLI = () => {
         seveKeyValue('sity', args.s)
     }
     if (args.t) {
-        seveKeyValue('token', args.t)
+        return saveToken(args.t)
     }
     //return wether
 }
